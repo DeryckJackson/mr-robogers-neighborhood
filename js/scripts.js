c829cc86443f73d67b6361aa894ffcf14de0d809
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-12 09:41:35 -0700

    add UI logic to output user input to the output section in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+
+
+
+// UI Logic
 $(document).ready(function() {
-  
+  $("form#input").submit(() => {
+    event.preventDefault();
+    const input = $("#number").val();
+
+    $("#output").text(input);
+    $(".hidden").show();
+  })
 });
