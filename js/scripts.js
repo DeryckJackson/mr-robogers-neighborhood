b85500dbe91709673228d343ddaba491548759ea
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-12 09:55:18 -0700

    implement first spec, outputs a range of numbers to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
+const mrRoboger = (number) => {
+  let outputString = "";
 
+  for (i = 0; i <= number; i++) {
+    outputString += `${i}, `;
+  }
+
+  return outputString.slice(0, -2);
+}
 
 
 // UI Logic
 $(document).ready(function() {
   $("form#input").submit(() => {
     event.preventDefault();
-    const input = $("#number").val();
+    const input = parseInt($("#number").val());
 
-    $("#output").text(input);
+    $("#output").text(mrRoboger(input));
     $(".hidden").show();
   })
 });
